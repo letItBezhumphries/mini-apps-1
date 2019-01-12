91bb51c109a8a9c58721c5feecfabfb9c6fa9d31
EricZH <letItBezhumphries>
2019-01-12 12:41:13 -0800

    started basic server

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,34 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var PORT = process.env.PORT || 3001;
+
+var app = express();
+
+app.use(express.static(__dirname, '/public'));
 
 
+
+app.listen(PORT, ()=> console.log(`app is listening on port ${PORT}`));
+
+
+
+
+/*
+*********************** model ************************** 
+managing of state
+*/
+
+
+/*
+************************ views ************************** 
+what the user sees 
+how state is displayed 
+*/
+
+
+
+
+/* 
+*********************** controller **********************
+user interface --forms, buttons, event listener
+*/
