1d7db92adbd50c7d0a68d83256d670bf27d08bea
EricZH <letItBezhumphries>
2019-01-18 02:58:29 -0800

    webpack added

---
+++ challenge_4/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: '/client/src/index.jsx',
+  module: {
+    rules: [
+      { 
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+   output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/dist'
+  }
+};
